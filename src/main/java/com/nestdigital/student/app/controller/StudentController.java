7d57f76032637ccd4bd307d9b56b32af929c2401
devika0999 <krishnadevika445@gmail.com>
2022-10-15 16:45:18 +0530

    routes

--- src/main/java/com/nestdigital/student/app/controller/StudentController.java
+++ src/main/java/com/nestdigital/student/app/controller/StudentController.java
@@ -1,4 +1,21 @@
 package com.nestdigital.student.app.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StudentController {
+
+
+    @GetMapping("/")
+    public String Homepage()
+    {
+        return "welome to my home page";
+    }
+
+    @GetMapping("/contact")
+    public String demo()
+    {
+        return"contact page";
+    }
 }
